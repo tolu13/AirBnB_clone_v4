eb4771e1587a65e778d38d8bf5a334201a9fd753
tolu13 <tolulopeolanrewaju8@gmail.com>
2024-03-03 14:46:19 -0800

    ok

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,17 +7,22 @@ from models import storage
 from api.v1.views import app_views
 from flask_cors import CORS
 import os
+from flasgger import Swagger
+from flasgger.utils import swag_from
 
 # Set flask server environmental setup
 host = os.getenv('HBNB_API_HOST', '0.0.0.0')
 port = os.getenv('HBNB_API_PORT', 5000)
 
 app = Flask(__name__)
+
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 # app_views BluePrint defined in api.v1.views
 app.register_blueprint(app_views)
 
 # Create a CORS instance
-cors = CORS(app, resources={r"/*": {"origins": '0.0.0.0'}})
+# cors = CORS(app, resources={r"/*": {"origins": '0.0.0.0'}})
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
